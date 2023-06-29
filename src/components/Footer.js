ae1de915677108cfdd02ddd5ded7856535564293
Vadim Davidov <vdvdavid21@gmail.com>
2023-06-29 20:36:15 +0300

    Change footer,button default color

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,12 @@
 import { useStoreState } from 'easy-peasy';
 
 function Footer() {
+  const today = new Date();
   const postCount = useStoreState(state => state.postCount);
   return (
     <footer className="Footer">
+      <p>Copyright &copy; {today.getFullYear()}</p>
+      <span>🗒</span>
       <p>{postCount} Blog Posts</p>
     </footer>
   );
