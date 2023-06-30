839a77f3ae7cd8b1bce40c5285ddd5c1bbfc63f8
Vadim Davidov <vdvdavid21@gmail.com>
2023-06-30 10:33:42 +0300

    Add links to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,18 @@
 import { useStoreState } from 'easy-peasy';
+import { Link } from 'react-router-dom';
 
 function Footer() {
   const today = new Date();
   const postCount = useStoreState(state => state.postCount);
   return (
     <footer className="Footer">
+      <p>
+        <Link to={'/'}>{postCount} Blog Posts</Link>
+      </p>
+      <span>
+        <Link to={'/post'}>🗒</Link>
+      </span>
       <p>Copyright &copy; {today.getFullYear()}</p>
-      <span>🗒</span>
-      <p>{postCount} Blog Posts</p>
     </footer>
   );
 }
